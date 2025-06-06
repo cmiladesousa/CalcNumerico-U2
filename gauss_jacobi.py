1bff14548def1a4186f3e072606c9192cff122b5
Camila de Sousa <cmiladesousa@gmail.com>
2025-06-06 10:24:02 -0300

    alteração para correção de erro

--- gauss_jacobi.py
+++ gauss_jacobi.py
@@ -28,32 +28,37 @@ def gauss_jacobi(matriz, b, x, e):
     iteracoes = 0
     n = matriz.shape[0]
 
-    if matrizDominante(matriz):
-        while (max(abs(np.array(dis_absoluta))) > e or dis_rel > e) and iteracoes < 500:
-            temp = []
-            dis_absoluta = []
-            for i in range(n):
-                aux = sum(matriz[i, j] * x[j] for j in range(n) if j != i)
-                novo_x = (1 / matriz[i, i]) * (b[i] - aux)
-                dis_absoluta.append(novo_x - x[i])
-                temp.append(novo_x)
+    matrizDominante(matriz)
+    while (max(abs(np.array(dis_absoluta))) > e or dis_rel > e) and iteracoes < 1000:
+        temp = []
+        dis_absoluta = []
+        for i in range(n):
+            aux = sum(matriz[i, j] * x[j] for j in range(n) if j != i)
+            #if np.isinf(aux) or np.isnan(aux):
+                #raise ValueError(f"Overflow ou NaN detectado na iteração {iteracoes}, linha {i}")
+            novo_x = (1 / matriz[i, i]) * (b[i] - aux)
+            #if np.isinf(novo_x) or np.isnan(novo_x):
+                #raise ValueError(f"Solução inválida na iteração {iteracoes}, linha {i}")
+            dis_absoluta.append(novo_x - x[i])
+            temp.append(novo_x)
 
-            dis_rel = max(abs(np.array(dis_absoluta))) / max(abs(np.array(temp)))
-            x = np.array(temp)
-            iteracoes += 1
+        dis_rel = max(abs(np.array(dis_absoluta))) / max(abs(np.array(temp)))
+        x = np.array(temp)
+        np.savetxt('vetorX.txt', x, fmt='%.6f')
+        iteracoes += 1
 
-        residuo(matriz, b, x)
-        print(f"Número de iterações: {iteracoes}")
-    else:
-        print("Encerrando execução devido à falta de dominância diagonal.")
+    residuo(matriz, b, x)
+    print(f"Número de iterações: {iteracoes}")
+    #else:
+        #print("Encerrando execução devido à falta de dominância diagonal.")
 
 
 # Dados de entrada
 matriz1 = mmread('bcsstk22.mtx')
 if not isinstance(matriz1, np.ndarray):
     matriz1 = matriz1.toarray()
-
-b1 = np.loadtxt('vetorB_22.txt')
+b1 = np.random.uniform(1, 50, 138)
+np.savetxt('vetorB_22.txt',  b1, fmt='%.6f')
 x1 = np.zeros_like(b1)
 
 matriz2 = mmread('bcsstk23.mtx')
@@ -80,7 +85,7 @@ b = np.array([7, -8, 6], dtype=float)
 x = np.array([0.7, -1.6, 0.6], dtype=float)
 e = 0.01
 
-gauss_jacobi(matriz, b, x, e)
-gauss_jacobi(matriz1, b1, x1, e)
-gauss_jacobi(matriz2, b2, x2, e)
+#gauss_jacobi(matriz, b, x, e)
+#gauss_jacobi(matriz1, b1, x1, e)
+#gauss_jacobi(matriz2, b2, x2, e)
 gauss_jacobi(matriz3, b3, x3, e)
